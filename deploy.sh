79b7d9cc00066a42515afb5cda891f3b76999d75
Ervinnne Sodusta <ervinnesodusta13@yahoo.com.ph>
2019-03-04 00:03:02 +0800

    Test updates with ftp

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ git config git-ftp.url "ftp://files.000webhost.com:21/public_html"
 git config git-ftp.user "gg-php-ervinne-01"
 git config git-ftp.password "DorisBaliw13"
 
-# Upload all files
+# Upload all files for the first time
 git ftp init
 
-# Or if the files are already there
+# Upload updates only
 git ftp catchup
 
-# Deploy (After you do your commits)
+# Deploy everything (After you do your commits)
 git ftp push
