7fdd4358473a7fd915a43eda617853d2d9d31d46
Ervinnne Sodusta <ervinnesodusta13@yahoo.com.ph>
2019-03-03 23:55:16 +0800

    Add deployment sample script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd /c/laragon/www/your-app-location
+
+# Setup
+git config git-ftp.url "ftp://files.000webhost.com"
+git config git-ftp.user "gg-php-ervinne-01"
+git config git-ftp.password "YourPasswordHere"
