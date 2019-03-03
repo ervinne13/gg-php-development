4e10e36ae7c596c670a81dad3eb2c90a6d4dea42
Ervinnne Sodusta <ervinnesodusta13@yahoo.com.ph>
2019-03-03 23:59:15 +0800

    Update ftp host to include port and public_html

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
-cd /c/laragon/www/your-app-location
+cd /c/laragon/www/gg-php-development
 
 # Setup
-git config git-ftp.url "ftp://files.000webhost.com"
+git config git-ftp.url "ftp://files.000webhost.com:21/public_html"
 git config git-ftp.user "gg-php-ervinne-01"
-git config git-ftp.password "YourPasswordHere"
+git config git-ftp.password "DorisBaliw13"
+
+# Upload all files
+git ftp init
+
+# Or if the files are already there
+git ftp catchup
+
+# Deploy (After you do your commits)
+git ftp push
